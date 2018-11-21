b1f8a27727c4e45a10d9e83c6e2083a8278eff3b
Ethan <a8843526@gmail.com>
2018-11-21 18:18:04 +0800

    add testing

---
+++ src/utility/check_file_test.go
@@ -0,0 +1,22 @@
+package utility
+
+import (
+	"testing"
+)
+
+func TestCheckMod(t *testing.T) {
+	var dirPath = "../flag"
+	var filePath = "../flag/flag.go"
+
+	if checkMod(&dirPath) == "dir" {
+		t.Log("utility.AcheckModdd PASS")
+	} else {
+		    t.Error("utility.AcheckModdd FAIL")
+	}
+
+	if checkMod(&filePath) == "file" {
+		t.Log("utility.AcheckModdd PASS")
+	} else {
+		    t.Error("utility.AcheckModdd FAIL")
+	}
+}
